58c6e58faaa47ea997eae5c7c88d6aad67545307
liwenguo123 <lguosky@gmail.com>
2016-05-15 00:03:42 -0400

    Update account.py

--- abcbank/account.py
+++ abcbank/account.py
@@ -9,7 +9,8 @@ class Account:
     def __init__(self, accountType):
         self.accountType = accountType
         self.transactions = []
-
+        self.latestWithdrawDate		# Added by liwenguo123
+        
     def deposit(self, amount):
         if (amount <= 0):
             raise ValueError("amount must be greater than zero")
@@ -21,6 +22,7 @@ class Account:
             raise ValueError("amount must be greater than zero")
         else:
             self.transactions.append(Transaction(-amount))
+            self.latestWithdrawDate = DateProvider.today();		# Added by liwenguo123
 
     def interestEarned(self):
         amount = self.sumTransactions()
@@ -30,12 +32,21 @@ class Account:
             else:
                 return 1 + (amount - 1000) * 0.002
         if self.accountType == MAXI_SAVINGS:
-            if (amount <= 1000):
-                return amount * 0.02
-            elif (amount <= 2000):
-                return 20 + (amount - 1000) * 0.05
-            else:
-                return 70 + (amount - 2000) * 0.1
+            # Revised by liwenguo123 
+			today = DateProvider.today()
+			withdrawPassedDays = today - self.latestWithdrawDate;
+			listDaysStr = str(withdrawPassedDays).split()
+			numDays = int(listDaysStr)
+			if (numDays > 10):
+				return amount * 0.05
+			else:
+				return amount * 0.001            
+            #if (amount <= 1000):
+            #    return amount * 0.02
+            #elif (amount <= 2000):
+            #    return 20 + (amount - 1000) * 0.05
+            #else:
+            #    return 70 + (amount - 2000) * 0.1
         else:
             return amount * 0.001
 
