3634343f46362dbaaf7aefc586162b0ba19f2499
liwenguo123 <lguosky@gmail.com>
2016-05-15 17:16:47 -0400

    Update transaction_tests.py

--- tests/transaction_tests.py
+++ tests/transaction_tests.py
@@ -3,6 +3,11 @@ from nose.tools import assert_is_instance
 from transaction import Transaction
 
 
-def test_type():
-    t = Transaction(5)
-    assert_is_instance(t, Transaction, "correct type")
+def test_type(amount): 
+	t = Transaction(amount) 
+	assert_is_instance(t, Transaction, "correct type") 
+
+def test_type_call(): 
+	amounts = [5, 1000, -200, None]
+	for amount in amounts:
+		test_type(amount)
